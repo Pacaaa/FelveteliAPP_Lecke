b32d7981c2b0984e39154f1ad9c34b5fdc4354fe
Dam Patrik <dampaco26@gmail.com>
2022-04-10 18:57:02 +0200

    javitas

--- APP/models.py
+++ APP/models.py
@@ -1,26 +1,9 @@
 
-from tkinter import CASCADE
+
 from django.db import models
 
 
        
-
-
-# class Iskola(models.Model):
-    
-#     # TODO: Define fields here
-
-#     class Meta:
-       
-#         verbose_name = 'Iskola'
-#         verbose_name_plural = 'Iskolas'
-
-#     iskola_diakigazolvanyszam=models.IntegerField()
-#     iskola=models.TextField()
-
-
-#     def __str__(self):
-#         return f"{self.iskola}"
     
 class DIAK(models.Model):
     
