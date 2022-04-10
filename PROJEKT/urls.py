405ef054e69504fb4cdc4eb7eb89d5c5ee8bbd3d
Dam Patrik <dampaco26@gmail.com>
2022-04-10 17:53:44 +0200

    valami2

--- PROJEKT/urls.py
+++ PROJEKT/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from APP.views import felvitel, megnezes
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',megnezes),
+    path('felvitel/',felvitel),
 ]
